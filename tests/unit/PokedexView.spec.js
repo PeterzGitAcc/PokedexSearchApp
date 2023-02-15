74704dc220ba2df53a07b3474147f8473c82539e
PeterzGitAcc <peter.chang.2020@smu.edu.sg>
2023-02-15 17:59:55 +0800

    added 1 unit test in

---
+++ tests/unit/PokedexView.spec.js
@@ -0,0 +1,14 @@
+import { shallowMount } from '@vue/test-utils'
+import PokedexView from '@/views/PokedexView.vue'
+
+//Test components exist
+describe('PokedexView.vue' , () => {
+    it('Renders all child components', () => {
+        const wrapper = mount(PokedexView)
+    
+        expect(wrapper.findComponent({name: "SearchBarComponent"}).exists()).toBe(true)
+        
+
+    })
+})
+//Test 
---
+++ tests/unit/app.spec.js
@@ -0,0 +1,11 @@
+import {mount} from '@vue/test-utils';
+import App from '@/App';
+
+describe('App.vue' , () => {
+    it('Renders NavbarComponent', () => {
+        const wrapper = mount(App)
+    
+        expect(wrapper.findComponent({name: "NavbarComponent"}).exists()).toBe(true)
+
+    })
+})
